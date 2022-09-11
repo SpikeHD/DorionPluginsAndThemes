b0bfe4d08ced5f266e2fb916c32f47de1be32c74
SpikeHD <spikegdofficial@gmail.com>
2022-09-10 21:54:00 -0700

    id plugin tweaks

--- plugins/IdBesideName/index.js
+++ plugins/IdBesideName/index.js
@@ -1,5 +1,9 @@
 let lastSavedChannel = 0
 
+/**
+ * TODO: REWRITE WITH MUTATION OBSERVER
+ */
+
 setInterval(() => {
   // Get all messages
   const msgs = document.querySelector('div[class*="messagesWrapper"]')?.querySelectorAll('li')
@@ -25,9 +29,14 @@ setInterval(() => {
       if (id) {
         // ...grab their username and shove a <span> next to it
         const uname = m.querySelector('span[class*="username"]')
+
+        // Do not add if the id is already there
+        if (uname.querySelector('.dorionId')) return
+
         const newSpan = document.createElement('span')
   
         newSpan.innerHTML = ` - ${id}`
+        newSpan.className = 'dorionId'
         newSpan.style.fontSize = '12px'
   
         uname.appendChild(
